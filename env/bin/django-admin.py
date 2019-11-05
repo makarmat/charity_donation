1875a9a30d66e804d1ea03ce007807978812a721
Mateusz Makarewicz <mateusz.makarewicz@interia.pl>
2019-11-05 22:32:51 +0100

    test

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mateusz/Pulpit/Zadania/Oddam_w_dobre_rece/charity_donation/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
